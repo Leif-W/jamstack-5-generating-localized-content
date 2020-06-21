5fddaa54ce9e00da8028a570099c42841934c3c2
Leif-W <leif.westerlind@gmail.com>
2020-06-21 12:32:23 -0400

    Add localized pages and redirects

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,18 +3,35 @@
 const axios = require( 'axios' );
 const dotenv = require( 'dotenv' );
 const result = dotenv.config( );
+// Need ./ in require to specify file
+const countries = require( './countries.json' );
 
 if ( result.error ) {
     throw result.error;
 }
+/*
+const locales = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
+*/
 
-const news_url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${ process.env.NEWS_API_KEY }`;
+const getNews = async country => {
+    const news_url = `https://newsapi.org/v2/top-headlines?country=${ country }&pageSize=5&apiKey=${ process.env.NEWS_API_KEY }`;
 
-module.exports = async ( ) => {
     try {
         const response = await axios.get( news_url );
-        return response.data;
+        return {
+	    'country': country,
+            'articles': response.data.articles,
+	}
     } catch ( error ) {
 	console.error( error );
     }
+};
+
+module.exports = async ( ) => {
+    const newsPromises = countries.map( getNews );
+
+    return Promise.all( newsPromises ).then( newsObjects => {
+        console.log( 'newsObjects: ', newsObjects );
+    return [ ].concat.apply( [ ], newsObjects );
+    } );
 };
