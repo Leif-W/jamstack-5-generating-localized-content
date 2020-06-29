c2960b5f8cd7987917efe5424e5cd21847645186
Leif-W <leif.westerlind@gmail.com>
2020-06-29 14:24:56 -0400

    Workaround for _redirect nunjucks permalink

--- src/_data/news.js
+++ src/_data/news.js
@@ -9,9 +9,6 @@ const countries = require( './countries.json' );
 if ( result.error ) {
     throw result.error;
 }
-/*
-const locales = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
-*/
 
 const getNews = async country => {
     const news_url = `https://newsapi.org/v2/top-headlines?country=${ country }&pageSize=5&apiKey=${ process.env.NEWS_API_KEY }`;
@@ -31,7 +28,7 @@ module.exports = async ( ) => {
     const newsPromises = countries.map( getNews );
 
     return Promise.all( newsPromises ).then( newsObjects => {
-        console.log( 'newsObjects: ', newsObjects );
-    return [ ].concat.apply( [ ], newsObjects );
+        //console.log( 'newsObjects: ', newsObjects );
+        return [ ].concat.apply( [ ], newsObjects );
     } );
 };
